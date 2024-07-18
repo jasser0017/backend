878b80d84beb39642a2143faaae701934494c8ba
unknown <jasserallela007@gmail.com>
2024-07-18 13:50:28 +0100

    first

---
+++ src/main/java/com/example/projet_fin_annee/ProjetFinAnneeApplication.java
@@ -0,0 +1,13 @@
+package com.example.projet_fin_annee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetFinAnneeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetFinAnneeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projet_fin_annee/ProjetFinAnneeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projet_fin_annee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetFinAnneeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
